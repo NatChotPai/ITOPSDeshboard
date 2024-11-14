c229ccba1ea41596ac71739756fc550fe265eca2
NatChotPai <145831360+NatChotPai@users.noreply.github.com>
2024-11-14 12:45:43 +0700

    init-v1

--- admin-dashboard/src/main.jsx
+++ admin-dashboard/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { React } from 'react'
+import { ReactDOM } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
